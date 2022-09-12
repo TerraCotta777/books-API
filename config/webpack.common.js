072ef74dcc5540d71e706f98f7428859e447bcd8
terracotta <begaiym.akylova@gmail.com>
2022-09-12 13:46:44 +0600

    Add project structure

---
+++ config/webpack.common.js
@@ -0,0 +1,87 @@
+const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const PATHS = {
+  src: path.join(__dirname, "../src"),
+  dist: path.join(__dirname, "../dist"),
+  assets: "assets/",
+};
+
+const PAGES_DIR = PATHS.src;
+const PAGES = fs
+  .readdirSync(PAGES_DIR)
+  .filter((filename) => filename.endsWith(".html"));
+
+module.exports = {
+  externals: {
+    paths: PATHS,
+  },
+  entry: {
+    app: PATHS.src,
+  },
+  output: {
+    path: PATHS.dist,
+    filename: `${PATHS.assets}js/[name].[contenthash].js`,
+    clean: true,
+    assetModuleFilename: "assets/[name][ext]",
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "../dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: function () {
+                  return [require("autoprefixer")];
+                },
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      "~": PATHS.src,
+      "@": `${PATHS.src}/js`,
+      images: PATHS.src + "/assets/img/",
+      icons: PATHS.src + "/assets/img/icons/",
+    },
+  },
+  plugins: [
+    ...PAGES.map(
+      (page) =>
+        new HtmlWebpackPlugin({
+          template: `${PAGES_DIR}/${page}`,
+          filename: `./${page}`,
+        })
+    ),
+  ],
+};
